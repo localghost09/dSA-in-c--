ba4e9b3832d35c24f9cf4ba3fad244434e032031
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-11 17:37:10 +0530

    init

---
+++ Lecture-2/DataTypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // This all are primitive data types
+   
+   // int data type stores the integer value;
+
+   int age = 25;  // 4bytes
+   cout<<age;
+   char grade = 'o';  //1 bytes
+   float Pi = 3.14;   // 4 bytes
+   bool age = true;   // 1bytes
+   double price = 22.00;     // 8 bytes
+
+
+
+    return 0;
+}
