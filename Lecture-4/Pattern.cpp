e51aeb58e1d464e0d9a8320cfc030e37285b127a
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-11 22:14:51 +0530

    init

--- Lecture-4/Pattern.cpp
+++ Lecture-4/Pattern.cpp
@@ -45,5 +45,33 @@ int main()
     //     cout << endl;
     // }
 
+    //
+    // 1
+    // 2 2
+    // 3 3 3
+
+    // int m = 3;
+    // for (int i = 0; i < m; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << i+1;
+    //     }
+    //     cout << endl;
+    // }
+
+    int n=4;
+    char ch='A';
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout<<ch <<" ";
+            
+            
+        }
+        ch++;
+        cout<<endl;
+    }
+
     return 0;
 }
