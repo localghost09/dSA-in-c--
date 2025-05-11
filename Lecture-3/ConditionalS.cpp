6f1e1e8e4efae2625d5347505770f2bc9283b8ce
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-11 17:52:46 +0530

    init

---
+++ Lecture-3/ConditionalS.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+   int n = 45;
+   if(n>0){
+    cout<<"number is positive"<<endl;
+
+   }else{
+    cout<<"negative"<<endl;
+   }
+
+
+
+
+    return 0;
+}
