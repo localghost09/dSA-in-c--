8c4a14e9be0ea8d0867335472dd2ccd3dd6dfe37
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-11 17:10:37 +0530

    init

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n,m;
+    cout<<"Enter a number  n"<<endl;
+    cin>>n;
+    cout<<"Enter a number m "<<endl;
+    cin>>m;
+
+    int sum = n+m;
+    cout<<"the sum of two numbwer is "<< sum <<endl;
+    return 0;
+}
