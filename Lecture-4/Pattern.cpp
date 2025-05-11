3f144760c3e3b198c2cc938d1199414a23edb765
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-11 21:13:30 +0530

    star Pattern

---
+++ Lecture-4/Pattern.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // int n;
+    // cin >> n;
+
+    // int num = 1;
+
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=n; j++){
+    //         cout<<num;
+    //         num++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // char ch = 'A';
+
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n; j++){
+    //         cout<<ch;
+    //         ch++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // cout<<"after pattern " <<ch;
+
+    // Star Pattern
+    //  *
+    //  * *
+    //  * * *
+
+    // int m = 3;
+    // for (int i = 0; i < m; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << " * ";
+    //     }
+    //     cout << endl;
+    // }
+
+    return 0;
+}
