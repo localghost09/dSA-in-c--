677717e7dec78ae8f04f2db9c5991a38ddebb1af
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-12 20:20:00 +0530

    Calculate sum of number from 1 to n

--- Lecture-5/function.cpp
+++ Lecture-5/function.cpp
@@ -2,16 +2,33 @@
 using namespace std;
 
 
-void printHello(){
-    cout<<"Hello"<<endl;
-}
+// void printHello(){
+//     cout<<"Hello"<<endl;
+// }
+
+// int sum(int a , int b){
+//     int s = a+b;
+//     return s;
+// }
+
+
+// Calculate sum of number from 1 to n;
+
+// int sumOFN(int n){
+//     int sum =0;
+//     for(int i=0; i<=n; i++){
+//         sum = sum +i;
+//     }
+//     return sum;
+// }
 
 
 int main(){
 
-    printHello();
-
-
+   // printHello();
+    //cout<< "sum of two number " <<sum(5,4)<<endl;
+    
+    //cout<<"Sum of N number are " <<sumOFN(5)<<endl;
 
     return 0;
 }
