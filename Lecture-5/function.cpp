4c284531a447bfa601d087ac19dfd422866c2c53
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-13 22:37:55 +0530

    calculate  ncr binomial coefficient for n & r

--- Lecture-5/function.cpp
+++ Lecture-5/function.cpp
@@ -41,6 +41,34 @@ using namespace std;
 //     cout<< " x = " <<x<<endl; 
 // }
 
+
+//calculate  ncr binomial coefficient for n & r
+
+// int factorial(int n){
+//     int fact = 1;
+//     for(int i=1; i<=n; i++){
+//         fact = fact*i;
+//     }
+
+//     return fact;
+// }
+
+
+// int nCr(int n , int r){
+//     int fact_n = factorial(n);
+//     int fact_r = factorial(r);
+//     int fact_nmr = factorial(n-r);
+
+//     return fact_n/(fact_r * fact_nmr);
+// }
+
+
+
+
+
+
+
+
 int main(){
 
    // printHello();
@@ -71,6 +99,15 @@ int main(){
     // }
 
     // cout<<digitSum;
+
+
+    // int n = 8;
+    // int r = 2;
+
+    // cout<<"calculate  ncr binomial coefficient for n & r "<< nCr(n,r)<<endl;
+    
+
+
 
     return 0;
 }
