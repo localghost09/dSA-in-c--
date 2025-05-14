35789a0d7487a76aaa812cfdb33686527f87d1b0
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-14 21:30:11 +0530

    init

--- Lecture-7/code.cpp
+++ Lecture-7/code.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int main() {
-    int a = 4 , b=8;
-    cout<<(a & b)<<endl;
-    cout<<(a|b)<<endl;
-    cout<<(a^b)<<endl;
+    // int a = 4 , b=8;
+    // cout<<(a & b)<<endl;
+    // cout<<(a|b)<<endl;
+    // cout<<(a^b)<<endl;
+
+
+    
     return 0;
 }
