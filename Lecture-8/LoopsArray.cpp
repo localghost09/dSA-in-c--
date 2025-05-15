ef3d6f0b1eca715e04b35b54d638811c09622ba7
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-15 18:27:29 +0530

    smallest value in array

---
+++ Lecture-8/LoopsArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    // Find smallest / largest in Array
+
+    // int arr[] = {3,12,-2,55};
+    // int size = sizeof(arr)/sizeof(arr[0]);
+    // int small = INT_MAX; // we can also use arr[0] instead of INT_MAX
+    // for(int i=0; i<size; i++){
+    //     if(arr[i]<small){
+    //         small = arr[i];
+    //     }
+        
+    // }
+
+    // cout<<small;
+
+
+
+
+    return 0;
+}
