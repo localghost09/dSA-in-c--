a6c7634293ad15e5462e6c8ff32085e54d9f89bd
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-15 20:43:58 +0530

    vector

---
+++ Lecture-9/vector.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> vec={1,2,3};
+    // for(int i:vec){
+    //     cout<<i <<endl;
+    // }
+
+    //Vector Functions 
+    //size
+    //cout<<"size = " <<vec.size() <<endl;
+
+    //push_back
+    // vec.push_back(35);
+    // // Print the updated vector
+    // for(int i:vec){
+    //     cout<<i <<endl;
+    // }
+    return 0;
+}
