5aa8a690c66c50929380b494ee09a6ad546df2c1
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-15 18:12:17 +0530

    Loops in Array

--- Lecture-8/Array.cpp
+++ Lecture-8/Array.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main() {
     
-    int marks[] = {99,100,54,36,88};
-    cout<<marks[0];
+    // int marks[] = {99,100,54,36,88};
+    // //cout<<marks[0];
+
+    // int size = sizeof(marks)/sizeof(marks[0]);
+
+    // for(int i=0; i<size; i++){
+    //     cout<<marks[i]<<endl;
+    // }
 
 
 
