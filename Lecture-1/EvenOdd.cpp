40a81c552e4ca4701b5bf03261e23f770fe27665
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-22 23:38:24 +0530

    Even Odd number

---
+++ Lecture-1/EvenOdd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter a number n : "<<endl;
+    cin>>n;
+    if(n%2==0){
+        cout<<"even number "<<endl;
+    }
+    else{
+        cout<<"odd number "<<endl;
+    }
+    return 0;
+}
