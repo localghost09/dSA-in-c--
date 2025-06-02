db8beb9ea66b19b2eadb2f42c19facb29a168c78
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-06-02 23:38:22 +0530

    all prime no

--- Lecture-5/HomeWORK/AllPrime.cpp
+++ Lecture-5/HomeWORK/AllPrime.cpp
@@ -1,7 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-    
+    int limit;
+    cout << "Enter the upper limit: ";
+    cin >> limit;
+
+    cout << "Prime numbers up to " << limit << " are:\n";
+
+    for (int num = 2; num <= limit; num++) {
+        bool isPrime = true;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            cout << num << " ";
+        }
+    }
+
+    cout << endl;
     return 0;
 }
